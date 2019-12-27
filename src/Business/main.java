dbfcd2680fa962aa0e1f0e3fb0be969c67b23a23
tuncay <info@tuncaycemuzun.com>
2019-12-28 00:36:49 +0300

    WordGame

---
+++ src/Business/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import GUI.mainGui;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author BaydoganMirac
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        mainGui a = new mainGui();
+    }
+    
+}
